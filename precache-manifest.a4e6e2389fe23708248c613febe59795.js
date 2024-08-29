3a6f4212e0088ed5d063991a129fbb7262049eb0
RohitGurkhe <154746480+RohitGurkhe@users.noreply.github.com>
2024-08-29 20:45:49 +0530

    Add files via upload

---
+++ precache-manifest.a4e6e2389fe23708248c613febe59795.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbfcab18e81fe00986793573ed87327f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ff94c900899a5254a299",
+    "url": "/static/css/main.f3a551e7.chunk.css"
+  },
+  {
+    "revision": "bd6bd55d0f17c3088198",
+    "url": "/static/js/2.b190f742.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.b190f742.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff94c900899a5254a299",
+    "url": "/static/js/main.9c9ffd3e.chunk.js"
+  },
+  {
+    "revision": "fd1e764c12408bbbbfc1",
+    "url": "/static/js/runtime-main.f859b388.js"
+  }
+]);
